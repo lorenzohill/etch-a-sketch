aa681a50900cc4dfa517580e50b66f6d70604227
lorenzohill <lorenzokhill985@gmail.com>
2022-07-17 14:05:31 -0500

    Added function to Change Canvas size

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const canvas = document.querySelector(".canvas")
-const canvasSize = 812
+let canvasSize = 812
 const clearButton = document.querySelector("#clear")
+const changeSizeButton = document.querySelector("#changeSize")
 
+changeSizeButton.addEventListener("click", changeSize)
 clearButton.addEventListener("click", clearBoard)
 
-for(let i = 0; i < 11128; i++){
-    const div = document.createElement("div")
-    div.classList.add("pixel")
-    div.addEventListener('mouseover',draw)
-    canvas.appendChild(div)
-    console.log("Div Created")
-}
+function createBoard(canvasSize){
 
+    for(let i = 0; i < canvasSize; i++){
+        const div = document.createElement("div")
+        div.classList.add("pixel")
+        div.addEventListener('mouseover',draw)
+        canvas.appendChild(div)
+        console.log("Div Created")
+    }
+    
+}
 
 function draw(Event){
     const div = document.querySelector("div")
@@ -21,6 +26,24 @@ function draw(Event){
 function clearBoard(){
     const div = document.querySelectorAll(".pixel")
     div.forEach(element => { element.classList.remove("dark")
+        
+    });
+}
+
+function changeSize(){
+    const div = document.querySelectorAll(".pixel")
+    canvasSize = parseInt(prompt("What Size?"))
+    console.log(canvasSize)
+    clearBoard()
+    deleteBoard()
+    createBoard(canvasSize)
+    return canvasSize
+}
+
+function deleteBoard(){
+    const div = document.querySelectorAll(".pixel")
+    div.forEach(element => {
+        element.remove()
         
     });
 }

b55c4dd955595161af5dcfd1df7d0454ece0f2d1
lorenzohill <lorenzokhill985@gmail.com>
2022-07-17 08:09:41 -0500

    Added CSS and MVP Javascript funcionality.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function main(){
-    
-}
-
 const canvas = document.querySelector(".canvas")
+const canvasSize = 812
 
-for(let i = 0; i < 50; i++){
+for(let i = 0; i < 128; i++){
     const div = document.createElement("div")
     div.classList.add("pixel")
+    div.addEventListener('mouseover',draw)
     canvas.appendChild(div)
     console.log("Div Created")
+}
+
+
+function draw(Event){
+    const div = document.querySelector("div")
+    Event.target.classList.add("dark")
 }

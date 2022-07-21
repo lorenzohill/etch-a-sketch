4b00bf51902f137e1c28a9a521b7cc146d0f1049
lorenzohill <lorenzokhill985@gmail.com>
2022-07-21 17:59:12 -0500

    Added max paramater

--- script.js
+++ script.js
@@ -10,18 +10,26 @@ clearButton.addEventListener("click", clearBoard)
 createBoard(canvasSize)
 
 function createBoard(canvasSize){
-
-    for(let i = 0; i < canvasSize * canvasSize; i++){
-        const div = document.createElement("div")
-        div.classList.add("pixel")
-        let resize = backgroundCanvas / canvasSize
-        div.style.width = resize + "px" 
-        div.addEventListener('mouseover',draw)
-        canvas.appendChild(div)
-        console.log("Div Created")
+    canvasSize = Math.abs(canvasSize)
+    if (canvasSize<=100){
+        for(let i = 0; i < canvasSize * canvasSize; i++){
+            const div = document.createElement("div")
+            div.classList.add("pixel")
+            let resize = backgroundCanvas / canvasSize
+            div.style.width = resize + "px" 
+            div.addEventListener('mouseover',draw)
+            canvas.appendChild(div)
+            console.log("Div Created")
+    }} else if (canvasSize <= 0 ) {
+        alert("Cannot be 0 or less! Please enter a positive number between 0 and 100")
+        createBoard(1)
+    } else {
+        alert("Cannot be more than 100! Please enter a positive number between 1 and 100")
+        createBoard(1)
     }
-    
-}
+    }
+
+
 
 function draw(Event){
     const div = document.querySelector("div")
